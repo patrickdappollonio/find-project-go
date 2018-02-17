ad6872f51cd20af1334128b3a6cd12a3fc7b46aa
Patrick D'appollonio <patrick@dappollonio.us>
2018-02-17 12:51:10 +0000

    Update code.

--- main.go
+++ main.go
@@ -2,77 +2,60 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
-	"strconv"
-	"strings"
+	"path/filepath"
+
+	"github.com/karrick/godirwalk"
+	"github.com/pkg/errors"
 )
 
-const (
-	sourcePath   = "%s/src"
-	vendorFolder = "vendor"
-	logOutput    = "FIND_PROJECT_LOG"
-)
+var gopath = os.Getenv("GOPATH")
 
-func main() {
-	// Create a logger
-	logger := log.New(ioutil.Discard, "[find-project] ", log.Lshortfile)
-
-	// Check if we have 2 args
-	if len(os.Args) != 2 {
-		errexit("Incorrect number of arguments passed")
-	}
-
-	// Enable logger if defined in the env var
-	if b, _ := strconv.ParseBool(os.Getenv(logOutput)); b {
-		logger.SetOutput(os.Stdout)
-	}
-
-	// Get the argument we need
-	searchingfor := os.Args[1]
-
-	// Try getting the $GOPATH
-	gopath := os.Getenv("GOPATH")
-
-	// Check if $GOPATH exists
-	if gopath == "" {
-		errexit("$GOPATH not set")
-	}
-
-	// Add the source path to the $GOPATH
-	gopath = fmt.Sprintf(sourcePath, strings.TrimSuffix(gopath, "/"))
-
-	// Open the directory for reading
-	_, err := os.Stat(gopath)
-
-	// Check for error
-	if err != nil {
-		if os.IsNotExist(err) {
-			errexit("$GOPATH path not found: %q", gopath)
-		}
-
-		errexit("Unknown error: %s", err.Error())
-	}
-
-	// Search for the folder recursively
-	pathfound := findpath(logger, gopath, searchingfor)
-
-	// Check if we did find a path
-	if pathfound == "" {
-		errexit("Unable to find folder %q in $GOPATH", searchingfor)
-	}
-
-	// Print the path to screen
-	fmt.Fprintln(os.Stdout, pathfound)
+type found struct {
+	Path string
 }
 
-func errexit(format string, args ...interface{}) {
-	if len(args) == 0 {
-		fmt.Fprintln(os.Stderr, format)
-	} else {
-		fmt.Fprintf(os.Stderr, format+"\n", args...)
+func (f *found) Error() string {
+	return "No error"
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		exit("Error: Additional parameter required: name of the folder to search for.")
 	}
 
-	os.Exit(-1)
+	searching := os.Args[1]
+	gopath = filepath.Join(gopath, "src")
+
+	err := godirwalk.Walk(gopath, &godirwalk.Options{
+		Callback: func(pathName string, de *godirwalk.Dirent) error {
+			if de.Name() == "vendor" || filepath.HasPrefix(de.Name(), ".") {
+				return filepath.SkipDir
+			}
+
+			fmt.Println(pathName)
+
+			if de.Name() != searching {
+				return nil
+			}
+
+			return &found{Path: pathName}
+		},
+	})
+
+	if err == nil {
+		exit(fmt.Sprintf("Error: Folder %q not found in $GOPATH", searching))
+	}
+
+	if data, ok := errors.Cause(err).(*found); ok {
+		fmt.Fprintln(os.Stdout, data.Path)
+		return
+	}
+
+	exit("Error:", err.Error(), fmt.Sprintf("Type: %T", err))
+}
+
+func exit(params ...interface{}) {
+	fmt.Fprintln(os.Stderr, params...)
+	os.Exit(1)
 }
